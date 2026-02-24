f669ed8d68b5ea1d6c9b45a0e76fdf7142cc6561
Victor <davik4life@gmail.com>
2026-02-24 17:11:55 +0100

    edited the serializer.py file

--- maintenance/serializers.py
+++ maintenance/serializers.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
 from .models import Asset, MaintenanceRecord, MaintenanceSchedule
-from typing import ClassVar
-
 
 class AssetSerializer(serializers.ModelSerializer):
     class Meta:
-        model: ClassVar[type] = Asset
-        fields: ClassVar[str]= '__all__'
-        read_only_fields: ClassVar[list[str]] = ["owner"]
-
+        model = Asset
+        fields = '__all__'
+        read_only_fields = ["owner"]
 
 class MaintenanceRecordSerializer(serializers.ModelSerializer):
     class Meta:
@@ -20,3 +17,4 @@ class MaintenanceScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = MaintenanceSchedule
         fields = '__all__'
+        read_only_fields = []
