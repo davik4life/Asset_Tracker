7a80bb658bacc49d02f86595b39cd0c58f02242a
Victor <davik4life@gmail.com>
2026-02-25 16:08:52 +0100

    Updated the maintenance/views.py ScheduleViewSet to ensure only self data is rendered

--- maintenance/views.py
+++ maintenance/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
 # from rest_framework import generics
 from .models import *
@@ -12,6 +13,7 @@ from datetime import date
 # Create your views here.
 class AssetViewSet(ModelViewSet):
     serializer_class = AssetSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self): #type : ignore
         return Asset.objects.filter(owner=self.request.user)
@@ -31,6 +33,7 @@ class AssetViewSet(ModelViewSet):
 
 class MaintenanceRecordViewSet(ModelViewSet):
     serializer_class = MaintenanceRecordSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self): #type : ignore
         return MaintenanceRecord.objects.filter(created_by=self.request.user)
@@ -39,6 +42,11 @@ class MaintenanceRecordViewSet(ModelViewSet):
         serializer.save(created_by=self.request.user)
 
 class ScheduleViewSet(ModelViewSet):
-    queryset = MaintenanceSchedule.objects.all()
     serializer_class = MaintenanceScheduleSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return MaintenanceSchedule.objects.filter(asset__owner=self.request.user)
+
+    # queryset = MaintenanceSchedule.objects.all()
+    
