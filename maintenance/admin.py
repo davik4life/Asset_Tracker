af6b7a994326259d525e72d18dba2debc6a7001a
Victor <davik4life@gmail.com>
2026-02-25 15:58:37 +0100

    Added the models to the Admin Dasboard for visibility and also updated a record by adding note and it worked okay.

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin  # type: ignore
+from .models import Asset, MaintenanceSchedule, MaintenanceRecord
 
 # Register your models here.
+admin.site.register(Asset)
+admin.site.register(MaintenanceSchedule)
+admin.site.register(MaintenanceRecord)
